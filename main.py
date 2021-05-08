888b5d1ce21d330a98b54c32759fcc98cad2b730
Orian Leitersdorf <orianl@campus.technion.ac.il>
2021-05-08 14:53:33 +0300

    Ex 29.

--- main.py
+++ main.py
@@ -145,11 +145,11 @@ def map_problem_experiments():
     #       3. save the visualization of the path in 'images/UCS_path_time_based.png'
     # You can use the code in the function 'toy_map_problem_experiment' for help.
     map_problem = MapProblem(streets_map, start_point, target_point, 'current_time')
-    uc = UniformCost()
-    res = uc.solve_problem(map_problem)
-    print(res)
-    file_path = os.path.join(Consts.IMAGES_PATH, 'UCS_path_time_based.png')
-    streets_map.visualize(path=res, file_path=file_path)
+    # uc = UniformCost()
+    # res = uc.solve_problem(map_problem)
+    # print(res)
+    # file_path = os.path.join(Consts.IMAGES_PATH, 'UCS_path_time_based.png')
+    # streets_map.visualize(path=res, file_path=file_path)
 
     # TODO [Ex.16]: create an instance of `AStar` with the `NullHeuristic` (implemented in
     #       `framework\graph_search\graph_problem_interface.py`),
@@ -186,10 +186,10 @@ def map_problem_experiments():
     #                   For more info see `problems/map_problem.py`.
     #               2. create an instance of `AStar` with the `ShortestPathsBasedHeuristic`,
     #                  solve the same `map_problem` with it and print the results (as before).
-    map_problem.set_additional_shortest_paths_based_data()
-    a_star = AStar(ShortestPathsBasedHeuristic)
-    res = a_star.solve_problem(map_problem)
-    print(res)
+    # map_problem.set_additional_shortest_paths_based_data()
+    # a_star = AStar(ShortestPathsBasedHeuristic)
+    # res = a_star.solve_problem(map_problem)
+    # print(res)
 
     # TODO [Ex.25]: 1. Call the function set_additional_history_based_data()
     #                   to set the additional history-based data in `map_problem`.
@@ -207,7 +207,11 @@ def map_problem_experiments():
     #       Use focal_epsilon=0.23, and max_focal_size=40.
     #       Use within_focal_priority_function=within_focal_h_sum_priority_function. This function
     #        (defined just above) is internally using the `HistoryBasedHeuristic`.
-    exit()  # TODO: remove!
+    map_problem.set_additional_history_based_data()
+    a_star_epsilon = AStarEpsilon(HistoryBasedHeuristic, within_focal_h_sum_priority_function,
+                                  focal_epsilon=0.23, max_focal_size=40)
+    res = a_star_epsilon.solve_problem(map_problem)
+    print(res)
 
 
 def run_all_experiments():
